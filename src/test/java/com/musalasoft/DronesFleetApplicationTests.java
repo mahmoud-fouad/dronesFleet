7ba91f603bd2857aa9aea0a88c063cc98cfe9907
mahmoud ahmed <mahmoud.ah.fouad@gmail.com>
2022-12-13 12:50:26 +0200

    crate spring boot project

---
+++ src/test/java/com/musalasoft/DronesFleetApplicationTests.java
@@ -0,0 +1,17 @@
+package com.musalasoft;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DronesFleetApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
